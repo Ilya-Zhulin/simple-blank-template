ce66d6ccb3f36fe73ffeb6b947583a5f2513281d
Ilya A. Zhulin <ilya.zhulin@hotmail.com>
2021-03-11 12:16:09 +0300

    Grid to content
    Grid fields to Main-content section were added.

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ if (isset($_POST['page_title'])) {
 		// Считаем позиции вo внутренних секциях
 		$inner_section_count = 0;
 		foreach ($sb_inner_sections_array as $sb_inner_sections_item) {
-			if ($this->countModules($sb_top_sections_item) || (isset($sections[$sb_top_sections_item]) && $sections[$sb_top_sections_item]['isExist'] > 0)) {
+			if ($this->countModules($sb_inner_sections_item) || (isset($sections[$sb_inner_sections_item]) && $sections[$sb_inner_sections_item]['isExist'] > 0)) {
 				$inner_section_count += 1;
 			}
 		}
@@ -75,9 +75,72 @@ if (isset($_POST['page_title'])) {
 					$out .= '>';
 				}
 				echo $out;
-				if ($sb1_show == 1 && ($this->countModules('sb-sidebar-a') || (isset($sections['sb-sidebar-a']) && $sections['sb-sidebar-a']['isExist'] > 0))) {
-					$sb1_real_width = $sb1_width;
-					if ($sb1_position == '1') {
+				if ($grid_main > 0) {
+					?>
+					<div uk-grid<?php echo $grid_attr_main . $grid_classes_main; ?>>
+						<?php
+					}
+					if ($sb1_show == 1 && ($this->countModules('sb-sidebar-a') || (isset($sections['sb-sidebar-a']) && $sections['sb-sidebar-a']['isExist'] > 0))) {
+						$sb1_real_width = $sb1_width;
+						if ($sb1_position == '1') {
+							?>
+							<aside id="sb-sidebar-a" class="sidebar-a uk-width-<?php echo $fraction($sb1_width); ?>@m" role="complementary">
+								<jdoc:include type="modules" name="sidebar-a" style=""/>
+								<?php
+								foreach ($sections['sb-sidebar-a'] as $sb_sidebar_a_position) {
+									if ($this->countModules($sb_sidebar_a_position['pos-name'])) {
+										echo _buildPosition($this, $sb_sidebar_a_position['pos-name'], $tplparams, $sections);
+									}
+								}
+								?>
+							</aside>
+							<?php
+						}
+					}
+					if ($sb2_show == 1 && ($this->countModules('sb-sidebar-b') || (isset($sections['sb-sidebar-b']) && $sections['sb-sidebar-b']['isExist'] > 0))) {
+						$sb2_real_width = $sb2_width;
+						if ($sb2_position == '1') {
+							?>
+							<aside id="sb-sidebar-b" class="sidebar-b uk-width-<?php echo $fraction($sb1_width); ?>@m" role="complementary">
+								<jdoc:include type="modules" name="sidebar-b" style=""/>
+								<?php
+								foreach ($sections['sb-sidebar-b'] as $sb_sidebar_b_position) {
+									if ($this->countModules($sb_sidebar_a_position['pos-name'])) {
+										echo _buildPosition($this, $sb_sidebar_a_position['pos-name'], $tplparams, $sections);
+									}
+								}
+								?>
+							</aside>
+							<?php
+						}
+					}
+					?>
+					<?php
+					if ($hidecomponent == 1) {
+						$content_width		 = $fraction(60 - $sb1_real_width - $sb2_real_width);
+//					JBDump(60 - $sb1_width - $sb2_width, 0);
+						$content_width_array = explode('-', $content_width);
+						if ($content_width_array[1] > 6) {
+							echo '<div class = "uk-width-1-1"><div class = "uk-alert uk-alert-warning"><i class = "uk-icon-exclamation-triangle uk-icon-small"></i> Sorry, content width class (uk-width-medium-' . $content_width . ') is not supported. Please change sidebar width to show correct grid.</div></div>';
+						} else {
+							?>
+							<main id="sb-content" class="uk-width-<?php echo $content_width; ?>" role="main">
+								<?php
+								if ($this->countModules('sb-main-top') || (isset($sections['sb-main-top']) && $sections['sb-main-top']['isExist'] > 0)) {
+									echo _buildPosition($this, 'sb-main-top', $tplparams, $sections);
+								}
+								echo '<jdoc:include type="message" />';
+								echo '<jdoc:include type="component" />';
+								if ($this->countModules('sb-main-bottom') || (isset($sections['sb-main-bottom']) && $sections['sb-main-bottom']['isExist'] > 0)) {
+									echo _buildPosition($this, 'sb-main-bottom', $tplparams, $sections);
+								}
+								?>
+							</main>
+							<?php
+						}
+					}
+					if ($sb1_show == 1 && ($this->countModules('sb-sidebar-a') || (isset($sections['sb-sidebar-a']) && $sections['sb-sidebar-a']['isExist'] > 0)) && $sb1_position == '0') {
+						$sb1_real_width = $sb1_width;
 						?>
 						<aside id="sb-sidebar-a" class="sidebar-a uk-width-<?php echo $fraction($sb1_width); ?>@m" role="complementary">
 							<jdoc:include type="modules" name="sidebar-a" style=""/>
@@ -91,10 +154,8 @@ if (isset($_POST['page_title'])) {
 						</aside>
 						<?php
 					}
-				}
-				if ($sb2_show == 1 && ($this->countModules('sb-sidebar-b') || (isset($sections['sb-sidebar-b']) && $sections['sb-sidebar-b']['isExist'] > 0))) {
-					$sb2_real_width = $sb2_width;
-					if ($sb2_position == '1') {
+					if ($sb2_show == 1 && ($this->countModules('sb-sidebar-b') || (isset($sections['sb-sidebar-b']) && $sections['sb-sidebar-b']['isExist'] > 0)) && $sb2_position == '0') {
+						$sb2_real_width = $sb2_width;
 						?>
 						<aside id="sb-sidebar-b" class="sidebar-b uk-width-<?php echo $fraction($sb1_width); ?>@m" role="complementary">
 							<jdoc:include type="modules" name="sidebar-b" style=""/>
@@ -108,60 +169,9 @@ if (isset($_POST['page_title'])) {
 						</aside>
 						<?php
 					}
-				}
-				?>
-				<?php
-				if ($hidecomponent == 1) {
-					$content_width		 = $fraction(60 - $sb1_real_width - $sb2_real_width);
-//					JBDump(60 - $sb1_width - $sb2_width, 0);
-					$content_width_array = explode('-', $content_width);
-					if ($content_width_array[1] > 6) {
-						echo '<div class = "uk-width-1-1"><div class = "uk-alert uk-alert-warning"><i class = "uk-icon-exclamation-triangle uk-icon-small"></i> Sorry, content width class (uk-width-medium-' . $content_width . ') is not supported. Please change sidebar width to show correct grid.</div></div>';
-					} else {
+					if ($grid_main > 0) {
 						?>
-						<main id="sb-content" class="uk-width-<?php echo $content_width; ?>" role="main">
-							<?php
-							if ($this->countModules('sb-main-top') || (isset($sections['sb-main-top']) && $sections['sb-main-top']['isExist'] > 0)) {
-								echo _buildPosition($this, 'sb-main-top', $tplparams, $sections);
-							}
-							echo '<jdoc:include type="message" />';
-							echo '<jdoc:include type="component" />';
-							if ($this->countModules('sb-main-bottom') || (isset($sections['sb-main-bottom']) && $sections['sb-main-bottom']['isExist'] > 0)) {
-								echo _buildPosition($this, 'sb-main-bottom', $tplparams, $sections);
-							}
-							?>
-						</main>
-						<?php
-					}
-				}
-				if ($sb1_show == 1 && ($this->countModules('sb-sidebar-a') || (isset($sections['sb-sidebar-a']) && $sections['sb-sidebar-a']['isExist'] > 0)) && $sb1_position == '0') {
-					$sb1_real_width = $sb1_width;
-					?>
-					<aside id="sb-sidebar-a" class="sidebar-a uk-width-<?php echo $fraction($sb1_width); ?>@m" role="complementary">
-						<jdoc:include type="modules" name="sidebar-a" style=""/>
-						<?php
-						foreach ($sections['sb-sidebar-a'] as $sb_sidebar_a_position) {
-							if ($this->countModules($sb_sidebar_a_position['pos-name'])) {
-								echo _buildPosition($this, $sb_sidebar_a_position['pos-name'], $tplparams, $sections);
-							}
-						}
-						?>
-					</aside>
-					<?php
-				}
-				if ($sb2_show == 1 && ($this->countModules('sb-sidebar-b') || (isset($sections['sb-sidebar-b']) && $sections['sb-sidebar-b']['isExist'] > 0)) && $sb2_position == '0') {
-					$sb2_real_width = $sb2_width;
-					?>
-					<aside id="sb-sidebar-b" class="sidebar-b uk-width-<?php echo $fraction($sb1_width); ?>@m" role="complementary">
-						<jdoc:include type="modules" name="sidebar-b" style=""/>
-						<?php
-						foreach ($sections['sb-sidebar-b'] as $sb_sidebar_b_position) {
-							if ($this->countModules($sb_sidebar_a_position['pos-name'])) {
-								echo _buildPosition($this, $sb_sidebar_a_position['pos-name'], $tplparams, $sections);
-							}
-						}
-						?>
-					</aside>
+					</div>
 					<?php
 				}
 				if (isset($tplparams['container_main']) && $tplparams['container_main'] !== '0') {
@@ -207,7 +217,7 @@ if (isset($_POST['page_title'])) {
 		}
 	}
 	?>
-    <script>
+	<script>
 		window.addEventListener('load', () =>{
 			quicklink.listen();
 		});
