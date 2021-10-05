20e7e69e100e1bc7cee67d3905fb6da0124c7e1a
Ilya A. Zhulin <ilya.zhulin@hotmail.com>
2021-10-05 16:24:30 +0300

    Подключение пользовательской favicon

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *
+ * Всевозможные добавки по условиям
+ *
+ */
+
+
+if (file_exists(JPATH_ROOT . 'path_to_theme_file')) {
+	include_once JPATH_ROOT . 'path_to_theme_file';
+}
---
+++ includes/head.php
@@ -0,0 +1,48 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die;
+//Less
+$less_acompile = $this->params->get('less_acompile', 0);
+//$less_custom_files	 = $this->params->get('less_custom_file', 'template.css');
+//$less_custom_files	 = (strpos($less_custom_files, ',') !== FALSE) ? explode(',', $less_custom_files) : $less_custom_files;
+?>
+
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta http-equiv="cleartype" content="on">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="HandheldFriendly" content="true">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<?php
+if (file_exists(JPATH_ROOT . 'path_to_theme_file1')) {
+	include_once JPATH_ROOT . 'path_to_theme_file1';
+}
+if (!JFile::exists(JPATH_ROOT . '/templates/simple_blank/images/favicon/favicon.ico')) {
+	?>
+	<link rel="icon" href="/templates/<?php echo $this->template ?>/images/template_favicon/favicon.svg">
+	<link rel="apple-touch-icon" sizes="180x180" href="/templates/<?php echo $this->template ?>/images/template_favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/templates/<?php echo $this->template ?>/images/template_favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/templates/<?php echo $this->template ?>/images/template_favicon/favicon-16x16.png">
+	<link rel="manifest" href="/templates/<?php echo $this->template ?>/images/template_favicon/site.webmanifest">
+	<link rel="mask-icon" href="/templates/<?php echo $this->template ?>/images/template_favicon/safari-pinned-tab.svg" color="#000000">
+	<meta name="msapplication-TileColor" content="#ada8a7">
+	<meta name="msapplication-TileImage" content="/templates/<?php echo $this->template ?>/images/template_favicon/mstile-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+<?php }
+<script src="/templates/simple_blank/js/quicklink.js"></script>
+<?php
+if ($less_acompile == 1) {
+	$less_path = '/templates/' . $this->template . '/less/';
+	?>
+<link rel="stylesheet/less" href="<?php echo $less_path; ?>template.less" />
+<script src="templates/<?php echo $this->template ?>/vendor/LessJS/less.min.js" ></script>
+	<?php
+}
+?>
+<?php
+if (file_exists(JPATH_ROOT . 'path_to_theme_file2')) {
+	include_once JPATH_ROOT . 'path_to_theme_file2';
+}
+?>
