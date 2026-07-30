6a4143a501b44864b90e69ea7cdf2e9dad0756ae
Илья Жулин <comeonrus@ya.ru>
2026-07-30 14:46:43 +0300

    Добавил новый макет модуля HTML

---
+++ html/mod_custom/clean.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * @package    DEV
+ * @version    __DEPLOY_VERSION__
+ * @author     Ilya A.Zhulin <ilya.zhulin@hotmail.com>
+ * @copyright  ©Ilya A.Zhulin, 2026
+ * @license    GNU General Public License version 2 or later;
+ *
+ * The last change: 21.07.2026, 12:02
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Uri\Uri;
+
+$moduleclass_sfx = strlen($params->get('moduleclass_sfx') > 0) ? ' ' . $params->get('moduleclass_sfx') : '';
+$modId           = 'mod-custom' . $module->id;
+
+if ($params->get('backgroundimage'))
+{
+	/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+	$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+	$wa->addInlineStyle('
+#' . $modId . '{background-image: url("' . Uri::root(true) . '/' . HTMLHelper::_('cleanImageURL', $params->get('backgroundimage'))->url . '");}
+', ['name' => $modId]);
+}
+
+?>
+
+<div id="<?php echo $modId; ?>" class="mod-custom<?php echo $moduleclass_sfx; ?>">
+	<?php echo $module->content; ?>
+</div>
