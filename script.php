021e56deeba81eb92ba4b46e3f8fe01a9d7dd99d
Илья Жулин <comeonrus@ya.ru>
2026-08-11 12:33:27 +0300

    Миграция при обновлении: досоздание папок скелета тем (images/fonts/html), чистка legacy бандлов theme-*.css (templates/css и media/css)

--- script.php
+++ script.php
@@ -13,6 +13,8 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Installer\InstallerScriptInterface;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Log\Log;
+use Joomla\Filesystem\File;
+use Joomla\Filesystem\Folder;
 use SimpleBlank\Site\Service\ThemeManager;
 
 class Tpl_SimpleBlankInstallerScript implements InstallerScriptInterface
@@ -56,6 +58,8 @@ class Tpl_SimpleBlankInstallerScript implements InstallerScriptInterface
 			return;
 		}
 
+		$this->migrateLegacyThemes();
+
 		$app = Factory::getApplication();
 		$template = $app->getTemplate(true);
 		$params = $template->params;
@@ -72,5 +76,61 @@ class Tpl_SimpleBlankInstallerScript implements InstallerScriptInterface
 		}
 
 		Log::add(Text::_('TPL_SIMPLE_BLANK_INSTALL_DONE'), Log::INFO, 'jerror');
+	}
+
+	/**
+	 * Миграция тем со старых версий шаблона:
+	 * - досоздаёт папки скелета темы (images, fonts, html + index.html),
+	 *   которые появились в новой версии;
+	 * - удаляет продовые бандлы theme-*.css старых схем (это копии,
+	 *   первоисточник всегда в папке темы).
+	 */
+	private function migrateLegacyThemes(): void
+	{
+		$themesRoot = JPATH_THEMES . '/simple_blank/themes';
+
+		if (is_dir($themesRoot))
+		{
+			foreach (glob($themesRoot . '/*', GLOB_ONLYDIR) ?: [] as $themePath)
+			{
+				$this->ensureThemeDirs($themePath);
+			}
+		}
+
+		// Устаревшие продовые бандлы старых версий (J3-корень шаблона и media)
+		foreach ([JPATH_THEMES . '/simple_blank/css', JPATH_ROOT . '/media/templates/site/simple_blank/css'] as $legacyCssPath)
+		{
+			if (!is_dir($legacyCssPath))
+			{
+				continue;
+			}
+
+			foreach (scandir($legacyCssPath) ?: [] as $file)
+			{
+				if (strpos($file, 'theme-') === 0)
+				{
+					@unlink($legacyCssPath . '/' . $file);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Досоздать недостающие папки скелета темы.
+	 *
+	 * @param   string  $themePath  Полный путь к папке темы
+	 */
+	private function ensureThemeDirs(string $themePath): void
+	{
+		foreach (['images', 'fonts', 'html'] as $dir)
+		{
+			if (is_dir($themePath . '/' . $dir))
+			{
+				continue;
+			}
+
+			Folder::create($themePath . '/' . $dir);
+			File::write($themePath . '/' . $dir . '/index.html', '<h1>&#128683; You are not welcome here</h1>');
+		}
 	}
 }
